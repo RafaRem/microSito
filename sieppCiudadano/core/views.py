d3cae7552c4e765017bfd9a47c44c0d3e46fd58d
RafaRem <edgarembao@hotmail.com>
2020-10-02 08:38:53 -0600

    condicion para covid

--- sieppCiudadano/core/views.py
+++ sieppCiudadano/core/views.py
@@ -49,7 +49,8 @@ class EjeView(View):
         informes = Informe.objects.filter(estatus = True )
         ejes = Eje.objects.filter(informe = inf, estatus = True)
         ejes = ejes.order_by('numero')
-        if eje.numero == '2' and inf.año == 2020:
+        print(inf.año)
+        if eje.numero == '2' and inf.año == '2020':
             visores = Visores.objects.filter(estatus=True)
             arrayvisores= []
             for visor in visores:
